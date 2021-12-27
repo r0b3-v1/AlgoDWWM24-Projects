bd23d8f9320c082e538f7b18e2753353189ef313
RBAGES <bagesremy@gmail.com>
2021-12-27 13:01:05 +0100

    added random colors to changeColor

--- public/js/changeColor.js
+++ public/js/changeColor.js
@@ -1,7 +1,16 @@
 //Matéo
+var elem = document.getElementById('para');
+
 function changeColor(newColor) {
-    var elem = document.getElementById('para');
     elem.style.color = newColor;
   }
   const contents = document.getElementsByClassName();
   const firstContent = contents[0];
+
+  function randomColor(){
+     var val1 = Math.floor(Math.random() * 255);
+     var val2 = Math.floor(Math.random() * 255);
+     var val3 = Math.floor(Math.random() * 255);
+     elem.style.color = 'rgb('+val1 + "," +val2+","+val3+")";
+    
+  }
